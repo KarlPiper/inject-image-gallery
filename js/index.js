06273d3054e22652ca90d14ed76723283daa7b0c
KarlPiper <9043540+KarlPiper@users.noreply.github.com>
2019-01-05 23:05:25 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,31 +1,58 @@
 var isRev = 0;
 var direction;
 
+//generate random thumbnails
 var i = 1;
 while (i < 10) {
-	var randomNumber = Math.floor((Math.random() * 400) + 200);
-	$(".right").append('<img class="right-inside" src="http://via.placeholder.com/'+randomNumber+'x' + randomNumber + '/1976D2/fff?text=' + i.toString() + '"/>');
+	var randomNumber = Math.floor(Math.random() * 400 + 200);
+	$(".right").append(
+		'<img class="right-inside" src="http://via.placeholder.com/' +
+			randomNumber +
+			"x" +
+			randomNumber +
+			"/1976D2/fff?text=" +
+			i.toString() +
+			'"/>'
+	);
 	i++;
 }
 
 /** triggers **/
 /* jump to first button */
 $(".jump-first").click(function() {
-	var jumpButton = $(this);
+	var firstThumb = $(".right-inside:first");
+	var lastThumb = $(".right-inside:last");
 	if (isRev) {
-		$(".right-inside:last").click();
+		if (lastThumb.hasClass("highlight")) {
+			return false;
+		} else {
+			lastThumb.click();
+		}
 	} else {
-		$(".right-inside:first").click();
+		if (firstThumb.hasClass("highlight")) {
+			return false;
+		} else {
+			firstThumb.click();
+		}
 	}
 });
 
 /* jump to last button */
 $(".jump-last").click(function() {
-	var jumpButton = $(this);
+	var firstThumb = $(".right-inside:first");
+	var lastThumb = $(".right-inside:last");
 	if (isRev) {
-		$(".right-inside:first").click();
+		if (firstThumb.hasClass("highlight")) {
+			return false;
+		} else {
+			firstThumb.click();
+		}
 	} else {
-		$(".right-inside:last").click();
+		if (lastThumb.hasClass("highlight")) {
+			return false;
+		} else {
+			lastThumb.click();
+		}
 	}
 });
 
@@ -33,6 +60,10 @@ $(".jump-last").click(function() {
 $(".sort-switch").click(function() {
 	$(".flex").toggleClass("reverse-order");
 	isRev = !isRev;
+});
+
+$(".show-thumbs").click(function() {
+	$('gallery').toggleClass('no-thumbnails');
 });
 
 /* arrow button clicked */
@@ -53,6 +84,18 @@ $(".left button").click(function() {
 		}
 	}
 });
+
+$('.fit').change(function() {
+	if ($(this :selected).val() === "sizing-fit-width") {
+		$('.left').attr('class', 'left fit-width')
+	} else if ($(this :selected).val() === "sizing-fit-height") {
+		$('.left').attr('class', 'left fit-height')
+	} else if ($(this :selected).val() === "sizing-fit-both") {
+		$('.left').attr('class', 'left fit-height fit-width')
+	} else {
+		$('.left').attr('class','left')
+	}
+})
 
 /* thumbnail clicked */
 $(".right-inside").click(function() {
@@ -137,6 +180,11 @@ function updateMainImg() {
 		height: mainImg.height(),
 		index: $("img.right-inside").index(activeThumbnail) + 1
 	};
+	transitionAnimation(activeThumbnail,mainImg,mainImgWrap,activeSrc);
+	updateInfo(imgData);
+}
+
+function transitionAnimation(activeThumbnail,mainImg,mainImgWrap,activeSrc) {
 	//simulate switching out for a new image
 	var widthAfter = activeThumbnail[0].naturalWidth;
 	var heightAfter = activeThumbnail[0].naturalHeight;
@@ -155,7 +203,6 @@ function updateMainImg() {
 			}
 		}
 	);
-	updateInfo(imgData);
 }
 
 function fadeIn() {
@@ -171,14 +218,42 @@ function updateInfo(data) {
 	$(".info").html(infoText.split("<")[0]);
 }
 
+$(".jump-over").click(function() {
+	var activeIndex = $(".highlight").index();
+	var skipThrough = 3;
+	var skipTo = $("img.right-inside:eq(" + (activeIndex + skipThrough) + ")");
+	$(".highlight").removeClass("highlight");
+	$('.pulseGrow').removeClass('pulseGrow');
+	$(skipTo).addClass("highlight");
+	updateMainImg();
+	activeIndex = $(".highlight").index();
+	skipFade(skipThrough, activeIndex);
+});
+
+
+function skipFade(skip,active) {
+	while (skip > 1) {
+		$("img.right-inside")
+			.eq(active - (skip - 1))
+			.addClass('pulseGrow')
+		skip--;
+	}
+}
+
+
 // TO DO LIST
-//   non looping mode, disable button when end is reached
 // ☑ better thumbnail change animation
 // ☑ updated main image size change animation
 // ☑ reverse sort order button
-// ☑ fit width/height
-//   stretch to fit option
-//   no thumbnails option
+// ☑ contain width/height
+// ☑ no thumbnails option
 // ☑ show image index
 // ☑ show image filename
 // ☑ jump to start/end buttons
+// ☑ skipped thumbnail animation
+// ☑ skip button stops at end then starts over
+// ☑ no thumbnail options if sidebar hidden
+// ☑ animation for hiding sidebar using CLASSES
+//   non looping mode, disable button when end is reached
+//   disable first/last buttons if already on first/last
+//   fix thumbnail hiding on vertical overflow
